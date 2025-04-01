ef31cd37df7f724ca17214c7ec39794ed72d581c
Ankit <anks.rane@gmail.com>
2025-04-01 17:57:58 +0530

    video added to bg

--- portfolio/src/Layout.jsx
+++ portfolio/src/Layout.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header/Header'
+import videoBg from './assets/video/bubble.mp4';
 
 function Layout() {
   return (
     <div className='main-wrapper'>
+      <div className="video-wrapper">
+        <video
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="video-background"
+        >
+          <source src={videoBg} type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+
       <Header />
         <main>
             <Outlet />
